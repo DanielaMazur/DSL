253b9c0cac3391255f7b57124986ccb5bd1b01e6
DanielaMazur <danielamazur2000@gmail.com>
2021-06-06 23:02:56 +0300

    Add 3 examples

--- generatedFile.py
+++ generatedFile.py
@@ -1,10 +1,9 @@
 import pysketcher as ps
 from pysketcher.backend.matplotlib import MatplotlibBackend
 figure = ps.Figure(-5.0, 5.0, -5.0, 5.0, MatplotlibBackend)
-from interpretation.Paralelogram import Paralelogram
-A = Paralelogram(figure)
-A.setAnglesDegrees(72.0)
-A.setEdgeLength(5.0,4.0)
-A.setNameAngles("1","2","3","4")
+from interpretation.Cube import Cube
+A = Cube(figure)
+A.drawDiagonal(3.0,5.0)
+A.setVertices("A","B","C","D","F","G","H","I")
 A.draw()
 figure.save('interpretation/assets/final.png')
